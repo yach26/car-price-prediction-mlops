36001d087d7a00e708ab857a896cb1f6e1fa556e
Yachna <sharmayachna40@gmail.com>
2026-06-09 02:02:00 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+e = '-e .'
+def get_requirements(file_path:str)->list[str]:
+    #RETURN THE LIST OF REQUIREMENTS
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+
+        if e in requirements:
+            requirements.remove(e)
+    return requirements
+    
+setup(
+    name='ML-Project',
+    version='0.0.1',
+    author='Yachna',
+    author_email='sharmayachna40@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
+    
